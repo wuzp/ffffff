5cdb2b5871c1e2e06fe1cac3c8e4d2f65de2f82a
wuzp <315136798@qq.com>
2013-10-29 16:08:49 +0800

    fsafsa

---
+++ Entities/AnalysisReport.cs
@@ -0,0 +1,26 @@
+﻿
+/****************************************************
+ * 
+ * 广联达软件股份有限责任公司版权所有.
+ * 
+ *****************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using GTP.Org.Entities;
+using GTP.Runtime.Biz.Service;
+using GTP.Runtime.Core.Loaders;
+using GTP.Runtime.DataObject;
+using GTP.Runtime.DataObject.Entity;
+using GTP.Runtime.DataObject.Entity.Collections;
+using GTP.Runtime.DataObject.Extensions;
+namespace GTP.Services.Analysis.Entities
+{
+	/// <summary>
+	/// 分析报表
+	/// </summary>
+	partial class AnalysisReport : BaseEntity
+	{
+	}
+}
